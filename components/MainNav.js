c936e3ad6170a2424d771c768780ec9e36154346
Deaca <77636742+HuuTinh2207@users.noreply.github.com>
2023-07-07 03:02:44 -0400

    Update 'active' class

--- components/MainNav.js
+++ components/MainNav.js
@@ -14,7 +14,7 @@ export default function MainNav() {
     function getSearchQuery(event) {
         setSearchQuery(event.target.value);
     }
-    
+
     function submitForm(event) {
         event.preventDefault();
         router.push(`/artwork?q=${encodeURIComponent(searchQuery)}`);
@@ -22,24 +22,24 @@ export default function MainNav() {
 
     return (
         <>
-        <Navbar expand="lg" className="bg-body-tertiary fixed-top" data-bs-theme="dark">
-            <Container>
-                <Navbar.Brand >Huu Tinh Luu</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Link href="/" passHref legacyBehavior><Nav.Link>Home</Nav.Link></Link> 
-                        <Link href="/search" passHref legacyBehavior><Nav.Link >Advanced Search</Nav.Link></Link>
-                    </Nav>
-                    <Form className="d-flex" onSubmit={submitForm}>
-                        <Form.Control onChange={getSearchQuery} type="search" placeholder="Search" className="me-2 bg-white" aria-label="Search"/>
-                        <Button type='submit' variant="success">Search</Button>
-                    </Form>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-        <br />
-        <br />
+            <Navbar expand="lg" className="bg-body-tertiary fixed-top" data-bs-theme="dark">
+                <Container>
+                    <Navbar.Brand >Huu Tinh Luu</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Link href="/" passHref legacyBehavior><Nav.Link className={router.pathname === "/" ? "active" : ""}>Home</Nav.Link></Link>
+                            <Link href="/search" passHref legacyBehavior><Nav.Link >Advanced Search</Nav.Link></Link>
+                        </Nav>
+                        <Form className="d-flex" onSubmit={submitForm}>
+                            <Form.Control onChange={getSearchQuery} type="search" placeholder="Search" className="me-2 bg-white" aria-label="Search" />
+                            <Button type='submit' variant="success">Search</Button>
+                        </Form>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <br />
+            <br />
         </>
     );
 }
